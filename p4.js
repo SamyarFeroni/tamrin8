c7903937f3d28e3ea41b05ba5148a6b47fe03bea
Samyar Feroni <s.feroni@avakatan.ir>
2022-11-27 21:07:28 +0330

    The new cods of fs

--- p4.js
+++ p4.js
@@ -1,12 +1,29 @@
 const fs = require('fs');
 
 fs.readFile('names.txt','utf8',function(err,data){
+    let list = [] 
+    let list2 = []
     if(err)console.log(err);
     else{
-        let list = [] ;
+
         data.toString()
-        console.log(data);
+       // console.log(data);
         list.push(data)
+   
         console.log(list);
     }
+    fs.readFile('numbers.txt','utf8',function(err,data2){
+        if(err)console.log(err);
+        else{
+            data2.toString();
+            //console.log(data2);
+            list2.push(data2);
+            console.log(list2)
+        }
+
+    })
+
+    console.log(list);
+   // console.log(list2);
+   
 })
