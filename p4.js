bc6c6c387b5bd05b92ce0b6b8848088669b8ee39
Samyar Feroni <s.feroni@avakatan.ir>
2022-11-28 16:23:09 +0330

    The new code of fs

--- p4.js
+++ p4.js
@@ -1,29 +1,66 @@
 const fs = require('fs');
 
-fs.readFile('names.txt','utf8',function(err,data){
-    let list = [] 
-    let list2 = []
-    if(err)console.log(err);
-    else{
 
-        data.toString()
-       // console.log(data);
-        list.push(data)
+// fs.readFile('names.txt','utf8',function(err,data){
+//     let list = [] 
+//     let list2 = []
+//     if(err)console.log(err);
+//     else{
+
+//         data.toString()
+//        // console.log(data);
+//         list.push(data)
    
-        console.log(list);
-    }
-    fs.readFile('numbers.txt','utf8',function(err,data2){
-        if(err)console.log(err);
-        else{
-            data2.toString();
-            //console.log(data2);
-            list2.push(data2);
-            console.log(list2)
-        }
+//         console.log(list);
+//     }})
 
-    })
 
-    console.log(list);
+ fs.readFile('numbers.txt','utf8',function(err,data2){
+
+    let text_ = data2.replace(/(\r)/gm,"");
+
+  let textByline = text_.split("\n");
+textByline.pop();
+
+
+
+  //split the id
+  let array = []
+  let ID = []
+  let number= []
+  let idAndNumber = {}
+  function changeType(valeu){
+    return Number(valeu)
+  }
+  for(let i= 0 ;i<textByline.length;i++){
+     //console.log(textByline[i]);
+     let IdAndNumber= textByline[i].split("-")
+     array.push(IdAndNumber)
+     ID.push(array[i][0]);
+     
+     number.push(array[i][1]);
+    
+     ID.forEach((element, index) => {
+        idAndNumber[element] = number[index]
+     })
+    // console.log(array[i][1]);
+  }
+//   ID.map(changeType);
+//   number.map(changeType)
+console.log(ID);
+console.log(number);
+  console.log(idAndNumber);
+ 
+ // console.log(array);
+
+});
+
+
+// console.log(text_num);
+// let text_numByLine = text_num.split('\n')
+// console.log(text_numByLine);
+
+
+
+   // console.log(list);
    // console.log(list2);
-   
-})
